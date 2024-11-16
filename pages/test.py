1f22b41663026362d8285f1eafe0a38bb47d1964
Jocelyn Velarde <70779495+JocelynVelarde@users.noreply.github.com>
2024-11-16 17:49:26 +0000

    Update database connection parameters to use more descriptive secret keys

--- pages/test.py
+++ pages/test.py
@@ -7,9 +7,9 @@ def create_connection():
     try:
         connection = mysql.connector.connect(
             host=st.secrets["db_host"],
-            user=st.secrets["db_user"],
+            user=st.secrets["db_username"],
             password=st.secrets["db_password"],
-            database=st.secrets["db_name"]
+            database=st.secrets["db_database"]
         )
         if connection.is_connected():
             return connection
