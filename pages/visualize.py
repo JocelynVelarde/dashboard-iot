ea3ca9738aca4b4092ab625de4a569c05ff83a94
JocelynVelarde <a01285780@itesm.mx>
2024-11-17 21:22:10 -0600

    sensor can be added from dashabord

--- pages/visualize.py
+++ pages/visualize.py
@@ -6,15 +6,21 @@ st.title("Dashboard")
 
 sensor_type = st.text_input(label="Add the sensor type")
 unit = st.text_input(label="Add unit")
-room_id = st.number_input(label="Add room id")
+room_id = st.number_input(label="Add room id", min_value=int(1), max_value=int(100))
 
 # Fetch data from the FastAPI endpoints
 response_houses = requests.get("http://127.0.0.1:8000/get-all-houses")
-response_add_sensor = requests.post("http://127.0.0.1:8000/add-sensor", json={
-    "sensor_type": sensor_type,
-    "unit": unit,
-    "room_id": room_id
-})
+
+if st.button(label='Add sensor'):
+    response_add_sensor = requests.post("http://127.0.0.1:8000/add-sensor", json={
+        "type_": sensor_type,
+        "unit": unit,
+        "room_id": room_id
+    })
+    if response_add_sensor.status_code == 200:
+        st.success("Sensor added!")
+    else: 
+        st.error(f"Failed to add sensor: {response_add_sensor.status_code}")
 
 if response_houses.status_code == 200:
     houses_data = response_houses.json()
@@ -22,9 +28,6 @@ if response_houses.status_code == 200:
 else:
     st.error(f"Failed to fetch data: {response_houses.status_code}")
 
-if response_add_sensor.status_code == 200:
-    st.success("Sensor added!")
-else: 
-    st.error(f"Failed to add sensor: {response_add_sensor.status_code}")
+
 
 
